a4d5a5db981a1d3a6af29da7b1cfae88cb6a2c56
muhamadd9 <mhmdramadan.192@gmail.com>
2025-01-08 05:23:47 +0200

    added email acctivation

--- src/Modules/auth/auth.controller.js
+++ src/Modules/auth/auth.controller.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/register", userService.register);
 router.post("/login", userService.login);
+router.get("/acctivate_account/:token", userService.acctivateAccount);
 
 export default router;
--- src/Modules/auth/auth.service.js
+++ src/Modules/auth/auth.service.js
@@ -2,6 +2,8 @@ import { userModel } from "../../DB/Models/user.model.js";
 import bcrypt from "bcrypt";
 import CryptoJS from "crypto-js";
 import jwt from "jsonwebtoken";
+import sendEmails, { subjects } from "../../utils/sendEmails.js";
+import { signUp } from "../../utils/generateHTML.js";
 
 export const register = async (req, res) => {
   try {
@@ -17,6 +19,15 @@ export const register = async (req, res) => {
       userName,
       phone: CryptoJS.AES.encrypt(phone, process.env.SECRET_KEY),
     });
+
+    // email
+    const token = jwt.sign({ email }, process.env.JWT_SECRET);
+    const link = `http://localhost:3000/auth/acctivate_account/${token}`;
+    const emailIsSent = await sendEmails({
+      to: email,
+      subject: subjects.register,
+      html: signUp(link),
+    });
     return res.status(201).json({ success: true, user });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
@@ -31,7 +42,10 @@ export const login = async (req, res) => {
       return res
         .status(400)
         .json({ success: false, message: "Email not valid" });
-
+    if (!user.isActivated)
+      return res
+        .status(400)
+        .json({ success: false, message: "Acctivate your account first" });
     const match = bcrypt.compareSync(password, user.password);
 
     if (!match)
@@ -48,5 +62,28 @@ export const login = async (req, res) => {
       .json({ success: true, message: "Login Success", token });
   } catch (error) {
     return res.status(500).json({ success: false, message: error?.message });
+  }
+};
+
+export const acctivateAccount = async (req, res) => {
+  try {
+    const { token } = req.params;
+    if (!token)
+      return res
+        .status(403)
+        .json({ success: false, message: "Token required" });
+
+    const { email } = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(email);
+    const user = await userModel.findOne({ email });
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    user.isActivated = true;
+    await user.save();
+    return res.status(200).json({ success: true, message: "Try to login" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
