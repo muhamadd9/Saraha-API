a69cecc608fd09276520c3a50c84a10437fef8f1
muhamadd9 <mhmdramadan.192@gmail.com>
2025-01-14 03:20:56 +0200

    added login schema validation

--- src/Modules/auth/auth.controller.js
+++ src/Modules/auth/auth.controller.js
@@ -2,16 +2,20 @@ import { Router } from "express";
 import * as userService from "./auth.service.js";
 import { asyncHandler } from "../../utils/errorHandling/asyncHandler.js";
 import { validation } from "../../middleware/validation.middleware.js";
-import { registerSchema } from "./auth.validation.js";
+import * as authSchemas from "./auth.validation.js";
 
 const router = Router();
 
 router.post(
   "/register",
-  validation(registerSchema),
+  validation(authSchemas.registerSchema),
   asyncHandler(userService.register)
 );
-router.post("/login", asyncHandler(userService.login));
+router.post(
+  "/login",
+  validation(authSchemas.loginSchema),
+  asyncHandler(userService.login)
+);
 router.get(
   "/acctivate_account/:token",
   asyncHandler(userService.acctivateAccount)
