2ad802527afc247e53f136114f9dab335a8da60b
muhamadd9 <mhmdramadan.192@gmail.com>
2025-01-21 14:44:01 +0200

    added cors package for deplyment

--- src/app.controller.js
+++ src/app.controller.js
@@ -4,10 +4,11 @@ import userController from "./Modules/user/user.controller.js";
 import messageController from "./Modules/messages/message.controller.js";
 import { globalErrorHandler } from "./utils/errorHandling/globalErrorHandlng.js";
 import { notFoundHandler } from "./utils/errorHandling/notFoundHandler.js";
-
+import cors from "cors";
 const bootstrap = (app, express) => {
-  app.use(express.json());
   connectDB();
+  app.use(cors());
+  app.use(express.json());
   app.use("/auth", authController);
   app.use("/user", userController);
   app.use("/message", messageController);
--- src/middleware/authoization.middleware.js
+++ src/middleware/authoization.middleware.js
@@ -1,9 +1,10 @@
 import CustomError from "../utils/errorHandling/customError.js";
 
 const isAuthorized = (...roles) => {
-  return  (req, res, next) => {
+  return (req, res, next) => {
     const { user } = req;
-    if (!roles.includes(user.role)) return next(new CustomError("Not authorized", 401));
+    if (!roles.includes(user.role))
+      return next(new CustomError("Not authorized", 401));
     return next();
   };
 };
